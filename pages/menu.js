0a3f9f8e11fce0f344773a275b36d2da1b57a142
kengo-taka <1117marine1117@gmail.com>
2021-05-26 14:46:37 -0700

    change menu

--- pages/menu.js
+++ pages/menu.js
@@ -1,224 +1,211 @@
 import styles from '../styles/Home.module.css'
-
+import Grid from '@material-ui/core/Grid'
 const Menu = () => {
   return (
-    <div>
+    <div className={styles.con}>
       <h1 align="center" className={styles.menuTitle}>Menu</h1>
 
 
 
 
+      <h1 className={styles.menuText1}>BEBIDAS</h1>
 
 
-      <h1>BEBIDAS
-   </h1>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={6}>
 
-      <h2>FRIASE</h2>
-
-      <h3>FRAPPES $49</h3>
-      <h4>Clásico $49</h4>
-      <h4>Caramel $49</h4>
-      <h4>Oreo $49</h4>
-      <h4>M&M's $56</h4>
-
-      <h3>SMOOTHIES</h3>
-      <h4>Lechera $49</h4>
-      <h4>Fresa $49</h4>
-      <h4>Plátano $49</h4>
-      <h4>Blueberry $49</h4>
-      <h4>Mango $49</h4>
-      <h4>Ferrero $56</h4>
-
-      <h3>MALTEADAS
-</h3>
-      <h4>Fresa $25</h4>
-      <h4>Chocolate $25</h4>
-      <h4>Vainilla $25</h4>
-
-      <h3>OTROS</h3>
-      <h4>Refresco (355ml) $20</h4>
-      <h4> Naranjada (regular o mineral) $25
-</h4>
-      <h4> Limonada (regular o mineral) $25
-</h4>
-      <h4>Agua embotellada (500ml) $15
-</h4>
-      <h4>Agua mineral Peñafiel (600ml) $20
-</h4>
-      <h4>Agua mineral Perrier (330ml) $30
-</h4>
-
-      <h3>SMOOTHIES</h3>
-      <h4>Lechera $49</h4>
-      <h4>Fresa $49</h4>
-      <h4>Plátano $49</h4>
-      <h4>Blueberry $49</h4>
-      <h4>Mango $49</h4>
-      <h4>Ferrero $56</h4>
-
-      <h3>SMOOTHIES</h3>
-      <h4>Lechera $49</h4>
-      <h4>Fresa $49</h4>
-      <h4>Plátano $49</h4>
-      <h4>Blueberry $49</h4>
-      <h4>Mango $49</h4>
-      <h4>Ferrero $56</h4>
-
-      <h2>CALIENTES
-</h2>
-      <h4>Americano (café Premium) $39</h4>
-      <h4>Americano refill $25
-</h4>
-      <h4> Cappuccino $45
-</h4>
-      <h4> Capuccino con sabor $49
-</h4>
-      <h4>Hot chocolate (con malvaviscos) $44</h4>
-      <h4>Honey Milk (con fresas, almendras y miel) $44
-</h4>
-      <h4>Té $30
-</h4>
-      <h4>Tisana $39
-</h4>
-
-      <h3>CERVEZAS
-</h3>
-      <h4>Corona $29</h4>
-      <h4>Vaso león rojo $19
-</h4>
-      <h4>Vaso michelado $15
-</h4>
-      <h4>Vaso chelado $15
-</h4>
-      <h4>Orden de botana $45</h4>
+          <h3 className={styles.menuText2}>FRAPPES</h3>
+          <div className={styles.menuText3}>
+            <h4>Clásico $49</h4>
+            <h4>Caramel $49</h4>
+            <h4>Oreo $49</h4>
+            <h4>M&M's $56</h4>
+          </div>
+          <h3 className={styles.menuText2}>SMOOTHIES</h3>
+          <div className={styles.menuText3}>
+            <h4>Lechera $49</h4>
+            <h4>Fresa $49</h4>
+            <h4>Plátano $49</h4>
+            <h4>Blueberry $49</h4>
+            <h4>Mango $49</h4>
+            <h4>Ferrero $56</h4>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+          <h3 className={styles.menuText2}>MALTEADAS</h3>
+          <div className={styles.menuText3}>
+            <h4>Fresa $25</h4>
+            <h4>Chocolate $25</h4>
+            <h4>Vainilla $25</h4>
+          </div>
+          <h3 className={styles.menuText2}>OTROS</h3>
+          <div className={styles.menuText3}>
+            <h4>Refresco (355ml) $20</h4>
+            <h4> Naranjada (regular o mineral) $25</h4>
+            <h4> Limonada (regular o mineral) $25</h4>
+            <h4>Agua embotellada (500ml) $15</h4>
+            <h4>Agua mineral Peñafiel (600ml) $20</h4>
+            <h4>Agua mineral Perrier (330ml) $30</h4>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+          <h2 className={styles.menuText2}>CALIENTES</h2>
+          <div className={styles.menuText3}>
+            <h4>Americano (café Premium) $39</h4>
+            <h4>Americano refill $25</h4>
+            <h4> Cappuccino $45</h4>
+            <h4> Capuccino con sabor $49</h4>
+            <h4>Hot chocolate (con malvaviscos) $44</h4>
+            <h4>Honey Milk (con fresas, almendras y miel) $44</h4>
+            <h4>Té $30</h4>
+            <h4>Tisana $39</h4>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+        <h3 className={styles.menuText2}>CERVEZAS</h3>
+          <div className={styles.menuText3}>
+            <h4>Corona $29</h4>
+            <h4>Vaso león rojo $19</h4>
+            <h4>Vaso michelado $15</h4>
+            <h4>Vaso chelado $15</h4>
+            <h4>Orden de botana $45</h4>
+          </div>
+        </Grid>
+      </Grid>
 
 
-      <h1>HAMBURGUESAS</h1>
+      <h1 className={styles.menuText1}>HAMBURGUESAS</h1>
 
-      <h3>CHICKEN BURGER $99</h3>
-      <h4>Deliciosa pechuga de pollo, jamon, queso americano, queso chihuahua y verdura</h4>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={6}>
+          <h3 className={styles.menuText4}>CHICKEN BURGER $99</h3>
+          <h4 className={styles.menuText3}>Deliciosa pechuga de pollo, jamon, queso americano, queso chihuahua y verdura</h4>
 
-      <h3>CHAMPI BURGER $99</h3>
-      <h4>Deliciosa carne de res, champiñones, queso crema, queso americano y verdura</h4>
+          <h3 className={styles.menuText4}>CHAMPI BURGER $99</h3>
+          <h4 className={styles.menuText3}>Deliciosa carne de res, champiñones, queso crema, queso americano y verdura</h4>
 
-      <h3>AMERICAN BURGER $99</h3>
-      <h4>Deliciosa carne de res, jamón, doble queso amarillo y verduras</h4>
+          <h3 className={styles.menuText4}>AMERICAN BURGER $99</h3>
+          <h4 className={styles.menuText3}>Deliciosa carne de res, jamón, doble queso amarillo y verduras</h4>
 
-      <h3>BACON BURGER $99</h3>
-      <h4>Deliciosa carne de res, tocino ahumado, queso americano, queso chihuahua y verdura</h4>
+          <h3 className={styles.menuText4}>BACON BURGER $99</h3>
+          <h4 className={styles.menuText3}>Deliciosa carne de res, tocino ahumado, queso americano, queso chihuahua y verdura</h4>
 
-      <h3>FRESCURA BURGER $99</h3>
-      <h4>Deliciosa carne de res, jamón, queso americano, queso chihuahua y verdura
-</h4>
+          <h3 className={styles.menuText4}>FRESCURA BURGER $99</h3>
+          <h4 className={styles.menuText3}>Deliciosa carne de res, jamón, queso americano, queso chihuahua y verdura</h4>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
 
+        </Grid>
+      </Grid>
 
+      <h1 className={styles.menuText1}>DESAYUNOS</h1>
 
+      <Grid container>
+        <Grid item xs={12} sm={12} md={6}>
+          <h3 className={styles.menuText4}>CHILAQUILES $99</h3>
+          <h4 className={styles.menuText3}>Rojos o verdes, acompañados de huevos estrellados y frijoles<br></br>Con pollo + $15</h4>
 
+          <h3 className={styles.menuText4}>OMELETTE $99</h3>
+          <h4 className={styles.menuText3}>Con base de queso más dos ingredientes a elegir: jamón, tocino, salchicha, champiñón. pimiento o cebolla</h4>
 
+          <h3 className={styles.menuText4}>HUEVOS AL GUSTO $99</h3>
+          <h4 className={styles.menuText3}>Divorciados, a la mexicana o revueltos con dos ingredientes</h4>
 
+          <h3 className={styles.menuText4}>HOT CAKES $99</h3>
+          <h4 className={styles.menuText3}> Ingrediente principal a elegir: Nutella, cajeta, lechera o miel de maple <br></br>Dos topping a elegir: fresas, durazno, plátano, nuez, almendras o M&m's</h4>
 
-      <h1>DESAYUNOS
-</h1>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
 
-      <h3>CHILAQUILES $99</h3>
-      <h4>Rojos o verdes, acompañados de huevos estrellados y frijoles<br></br>Con pollo + $15</h4>
-
-      <h3>OMELETTE $99</h3>
-      <h4>Con base de queso más dos ingredientes a elegir: jamón, tocino, salchicha, champiñón. pimiento o cebolla</h4>
-
-      <h3>HUEVOS AL GUSTO $99</h3>
-      <h4>Divorciados, a la mexicana o revueltos con dos ingredientes</h4>
-
-      <h3>HOT CAKES $99</h3>
-      <h4> Ingrediente principal a elegir: Nutella, cajeta, lechera o miel de maple <br></br>Dos topping a elegir: fresas, durazno, plátano, nuez, almendras o M&m's</h4>
-
-
-
-      <h1>PIZZAS
-</h1>
-      <p>Chica: 30cm - Grande: 40cm</p>
-
-      <h3>Pepperoni y
-      champiñones
-</h3>
-      <p>Chica $99, Grande $144</p>
+        </Grid>
+      </Grid>
 
 
-      <h3>Hawaiana</h3>
-      <p>Chica $99, Grande $139</p>
-      <h4>(jamón y piña)</h4>
+      <h1 className={styles.menuText1}>PIZZAS</h1>
+      <p className={styles.menuText3}>Chica: 30cm - Grande: 40cm</p>
 
-      <h3>Freschezza</h3>
-      <p>Chica $99, Grande $139</p>
-      <h4>(champiñon, pimiento, jitomate, cebolla y aceituna)</h4>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={6}>
+          <h3 className={styles.menuText4}>Pepperoni y champiñones</h3>
+          <p className={styles.menuText3}>Chica $99, Grande $144</p>
+          <h3 className={styles.menuText4}>Hawaiana</h3>
+          <p className={styles.menuText3}>Chica $99, Grande $139</p>
+          <h4 className={styles.menuText3}>(jamón y piña)</h4>
 
-      <h3>Cuatro quesos</h3>
-      <p>Chica $89, Grande $139</p>
-      <h4>(Queso crema, chihuahua, americano y parmesano)</h4>
+          <h3 className={styles.menuText4}>Freschezza</h3>
+          <p className={styles.menuText3}>Chica $99, Grande $139</p>
+          <h4 className={styles.menuText3}>(champiñon, pimiento, jitomate, cebolla y aceituna)</h4>
 
-      <h3>Chicken BBO</h3>
-      <p>Chica $139, Grande $209</p>
-      <h4>(fajitas de pollo, jamón, cebolla morada, champiñón y nuestra rica salsa BBO)</h4>
+          <h3 className={styles.menuText4}>Cuatro quesos</h3>
+          <p className={styles.menuText3}>Chica $89, Grande $139</p>
+          <h4 className={styles.menuText3}>(Queso crema, chihuahua, americano y parmesano)</h4>
 
-      <h3>Chipocrema</h3>
-      <p>Chica $129, Grande $199</p>
-      <h4>(fajitas de pollo, tocino, champiñones, jitomate y nuestra famosa salsa Chipocrema)</h4>
+          <h3 className={styles.menuText4}>Chicken BBO</h3>
+          <p className={styles.menuText3}>Chica $139, Grande $209</p>
+          <h4 className={styles.menuText3}>(fajitas de pollo, jamón, cebolla morada, champiñón y nuestra rica salsa BBO)</h4>
 
-      <h3>Prime</h3>
-      <p>Chica $149, Grande $219</p>
-      <h4>(champiñones, pimiento, jitomate, pepperoni, cebolla morada, aceitunas, salchicha, jamón y tocino) </h4>
+          <h3 className={styles.menuText4}>Chipocrema</h3>
+          <p className={styles.menuText3}>Chica $129, Grande $199</p>
+          <h4 className={styles.menuText3}>(fajitas de pollo, tocino, champiñones, jitomate y nuestra famosa salsa Chipocrema)</h4>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+          <h3 className={styles.menuText4}>Prime</h3>
+          <p className={styles.menuText3}>Chica $149, Grande $219</p>
+          <h4 className={styles.menuText3}>(champiñones, pimiento, jitomate, pepperoni, cebolla morada, aceitunas, salchicha, jamón y tocino) </h4>
 
-      <h3>Carnival</h3>
-      <p>Chica $129, Grande $199</p>
-      <h4>(pepperoni, salchicha, tocino y jamón)</h4>
+          <h3 className={styles.menuText4}>Carnival</h3>
+          <p className={styles.menuText3}>Chica $129, Grande $199</p>
+          <h4 className={styles.menuText3}>(pepperoni, salchicha, tocino y jamón)</h4>
 
-      <h3>Pastor</h3>
-      <p>Chica $139, Grande $209</p>
-      <h4>(carne al pastor. piña, cebolla y cilantro) </h4>
+          <h3 className={styles.menuText4}>Pastor</h3>
+          <p className={styles.menuText3}>Chica $139, Grande $209</p>
+          <h4 className={styles.menuText3}>(carne al pastor. piña, cebolla y cilantro) </h4>
 
-      <h3>Española</h3>
-      <p>Chica $149, Grande $219</p>
-      <h4>(jamón serrano, queso crema, jitomate y albahaca)</h4>
+          <h3 className={styles.menuText4}>Española</h3>
+          <p className={styles.menuText3}>Chica $149, Grande $219</p>
+          <h4 className={styles.menuText3}>(jamón serrano, queso crema, jitomate y albahaca)</h4>
 
+          <h3 className={styles.menuText4}>Del mar</h3>
+          <p className={styles.menuText3}>Chica $149, Grande $219</p>
+          <h4 className={styles.menuText3}>(camarones, aguacate y cebolla morada)</h4>
 
-      <h3>Del mar</h3>
-      <p>Chica $149, Grande $219</p>
-      <h4>(camarones, aguacate y cebolla morada)</h4>
+        </Grid>
+      </Grid>
 
-
-      <h1>CREPAS
-</h1>
-      <h2>DULCES</h2>
-      <p>(1 o 2 ingredientes a elegir) $59</p>
-      <p>*Ingrediente extra: $10</p>
-      <h3>Nutella</h3>
-      <h3>Fresas</h3>
-      <h3>Cajeta</h3>
-      <h3>Lechera</h3>
-      <h3>Durazno</h3>
-      <h3>Plátano</h3>
-      <h3>Queso crema</h3>
-      <h3>Nuez</h3>
-      <h3>Almendras</h3>
-      <h3>M&m's</h3>
-      <h3>Mermelada de fresa o zarzamora</h3>
-
-
-      <h2>SALADAS</h2>
-      <p>Queso mas 1 ingrediente a elegir) $69</p>
-      <p>*Ingrediente extra: $15</p>
-      <h3>Nutella</h3>
-      <h3>Fresas</h3>
-      <h3>Cajeta</h3>
-      <h3>Lechera</h3>
-      <h3>Durazno</h3>
-      <h3>Plátano</h3>
-      <h3>Queso crema</h3>
-      <h3>Nuez</h3>
-      <h3>Almendras</h3>
-      <h3>M&m's</h3>
-      <h3>Mermelada de fresa o zarzamora</h3>
-
+      <h1 className={styles.menuText1}>CREPAS</h1>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={6}>
+          <h2 className={styles.menuText2}>DULCES</h2>
+          <p className={styles.menuText3}>(1 o 2 ingredientes a elegir) $59</p>
+          <p className={styles.menuText3}>*Ingrediente extra: $10</p>
+          <div className={styles.menuText3}>
+          <h3>Nutella</h3>
+          <h3>Fresas</h3>
+          <h3>Cajeta</h3>
+          <h3>Lechera</h3>
+          <h3>Durazno</h3>
+          <h3>Plátano</h3>
+          <h3>Queso crema</h3>
+          <h3>Nuez</h3>
+          <h3>Almendras</h3>
+          <h3>M&m's</h3>
+          <h3>Mermelada de fresa o zarzamora</h3>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+          <h2 className={styles.menuText2}>SALADAS</h2>
+          <p className={styles.menuText3}>Queso mas 1 ingrediente a elegir) $65</p>
+          <p className={styles.menuText3}>*Ingrediente extra: $15</p>
+          <div className={styles.menuText3}>
+          <h3>Pollo</h3>
+          <h3>Salchicha</h3>
+          <h3>Jamón</h3>
+          <h3>Pepperoni</h3>
+          <h3>Jamón serrano</h3>
+          <h3>Champiñones</h3>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
